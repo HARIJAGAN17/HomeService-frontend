8cad9820e75aa2cbae9c3336a78747605ea7b699
Harijagan.T <harijagan.t@ust.com>
2024-09-06 06:12:13 +0530

    decoded jwt token

--- src/app/services/loginservices/loginservice.service.ts
+++ src/app/services/loginservices/loginservice.service.ts
@@ -24,4 +24,27 @@ export class LoginserviceService {
     console.log(localStorage.getItem('token'));
     return localStorage.getItem('token');
    }
+
+   haveAccess(){
+    
+    var logginToken = localStorage.getItem("token");
+
+    if (logginToken) {
+        var _extractedToken = logginToken.split(".")[1];
+
+        try {
+            var _atobData = atob(_extractedToken);
+            var _finalData = JSON.parse(_atobData);
+            console.log(_finalData);
+            return _finalData.UserRole;
+        } catch (error) {
+            console.error("Error decoding token:", error);
+            return null;
+        }
+    } else {
+        console.error("No token found in localStorage.");
+        return null;
+    }
+}
+
 }
--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -13,6 +13,7 @@ export class AuthGuard implements CanActivate {
   canActivate() {
     // Implement your authentication logic here
     if(this.loginService.isLoggedIn()){
+      this.loginService.haveAccess();
       return true;
     }
    else{
