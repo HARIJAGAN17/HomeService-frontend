94bb71d4afa5a4c88de26cfa90824ab17bb4d0a3
Harijagan.T <harijagan.t@ust.com>
2024-09-08 19:43:14 +0530

    displaying all services in booking with book button

--- src/app/components/customerPage/bookservice/bookservice.component.ts
+++ src/app/components/customerPage/bookservice/bookservice.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProviderCrudService } from '../../../services/provider/provider-crud.service';
+import { ServiceResponse } from '../../../model/serviceget';
 
 @Component({
   selector: 'app-bookservice',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './bookservice.component.css'
 })
 export class BookserviceComponent {
+  allServices: ServiceResponse[] = []; 
+  filteredServices: ServiceResponse[] = []; 
+  categories: string[] = []; 
 
+  constructor(private providerService: ProviderCrudService) {}
+
+  ngOnInit(): void {
+    this.providerService.getServices().subscribe({
+      next: (responseData: ServiceResponse[]) => {
+        this.allServices = responseData;
+        this.filteredServices = responseData; 
+        this.categories = [...new Set(responseData.map(service => service.category))];
+      },
+      error: (error) => {
+        if (error.status === 401) {
+          console.log("Unauthorized");
+        }
+      }
+    });
+  }
+
+  onCategoryChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedCategory = selectElement.value;
+
+    if (selectedCategory === '') {
+      this.filteredServices = this.allServices; 
+    } else {
+      this.filteredServices = this.allServices.filter(service => service.category === selectedCategory);
+    }
+  }
 }
