f5433dd13e84e17e61d483a0e123c546c285f2ae
Harijagan.T <harijagan.t@ust.com>
2024-09-12 23:29:48 +0530

    designing progress landing page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private viewportScroller: ViewportScroller,private route:Router) {}
 
+  scrollTo(section: string): void {
+
+    this.route.navigateByUrl("/home/landing");
+    setTimeout(() => {
+      const element = document.getElementById(section);
+      if (element) {
+        window.scrollTo({
+          top: element.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    }, 100);
+  }
+  
 }
