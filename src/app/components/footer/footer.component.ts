ddd4ba57e822a4aa69302067fe4c0299bdfd4081
Harijagan.T <harijagan.t@ust.com>
2024-09-12 23:33:30 +0530

    updated footer fucntionality

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  constructor(private viewportScroller: ViewportScroller,private route:Router) {}
+
+  scrollTo(section: string): void {
+
+    this.route.navigateByUrl("/home/landing");
+    setTimeout(() => {
+      const element = document.getElementById(section);
+      if (element) {
+        window.scrollTo({
+          top: element.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    }, 100);
+  }
 }
